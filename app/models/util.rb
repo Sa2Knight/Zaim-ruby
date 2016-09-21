c73993c325d1d19b9e583b7a94e308969a21cbe2
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-09-22 05:28:16 +0900

    年月日を年月に変換するメソッドを実装

--- app/models/util.rb
+++ app/models/util.rb
@@ -24,4 +24,22 @@ class Util
     return url
   end
 
+  # yyyy-mm-dd を yyyy-mmに変換する
+  # cutoffを指定することで、25日以降を翌月扱いにできる
+  #--------------------------------------------------------------------
+  def self.to_month(date , cutoff = 25)
+    match = date.match(/([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})/)
+    year = match[1].to_i
+    month = match[2].to_i
+    day = match[3].to_i
+    if cutoff <= day
+      month += 1
+      if month == 13
+        year += 1
+        month = 1
+      end
+    end
+    return "#{year}-#{month}"
+  end
+
 end
