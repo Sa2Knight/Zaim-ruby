158afbf511356763c329f43cc3c89a0d5dacc753
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-09-22 18:54:00 +0900

    月々の食費・ガス代・電気代・水道代の一覧を表示する機能を実装

--- app/zaim_controller.rb
+++ app/zaim_controller.rb
@@ -13,6 +13,27 @@ class ZaimController < Sinatra::Base
     erb :index
   end
 
+  # /monthly - 月ごとの集計を表形式で表示
+  #--------------------------------------------------------------------
+  get '/monthly/?' do
+    zaim_params = {}
+    category = params['category']
+    genre = params['genre']
+    if category
+      @target = category
+      category_id = @zaim.category_to_id(category)
+      zaim_params['category_id'] = category_id
+    elsif genre
+      @target = genre
+      genre_id = @zaim.genre_to_id(genre)
+      zaim_params['genre_id'] = genre_id
+    else
+      @target = "累計"
+    end
+    @monthly = @zaim.monthly_spending(zaim_params)
+    erb :monthly
+  end
+
   # before - 全てのURLにおいて初めに実行される
   #---------------------------------------------------------------------
   before do
