68b3499c5f633f119b1a723046e40b317e0f3908
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-09-24 21:05:30 +0900

    Web版zaimの履歴検索ページのURLを生成する関数を実装

--- app/public/js/zaim.js
+++ app/public/js/zaim.js
@@ -1,1 +1,10 @@
 var base = "https://zaim.net/money";
+
+function createURL(params) {
+  var url = base + "?";
+  Object.keys(params).forEach(function(k) {
+    var value = params[k];
+    url += k + "=" + value + "&";
+  });
+  return url;
+}
