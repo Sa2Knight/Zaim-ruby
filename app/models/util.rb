4d89026bbde5f73ac3e039d68b843f88685962b0
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-01-09 18:31:26 +0900

    月の始まりを25日から1日に変更

--- app/models/util.rb
+++ app/models/util.rb
@@ -28,28 +28,14 @@ class Util
   end
 
   # yyyy-mm-dd を yyyy-mmに変換する
-  # cutoffを指定することで、25日以降を翌月扱いにできる
   #--------------------------------------------------------------------
-  def self.to_month(date , cutoff = 25)
+  def self.to_month(date)
     # 文字列を年月日に分割
     match = date.match(/(\d{4})-(\d{1,2})-(\d{1,2})/)
     year = match[1].to_i
     month = match[2].to_i
     day = match[3].to_i
-
-    # 当該月cutoff日が土日の場合、cutoff値をズラす
-    date_object = Date.new(year , month , cutoff)
-    date_object.wday == 0 && cutoff -= 2
-    date_object.wday == 6 && cutoff -= 1
-
-    # cutoff値を元に月を補正
-    if cutoff <= day
-      month += 1
-      if month == 13
-        year += 1
-        month = 1
-      end
-    end
+    date_object = Date.new(year , month , 1)
     return "#{year}-#{month}"
   end
 
